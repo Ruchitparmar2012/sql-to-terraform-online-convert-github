cff6f5f4780f8e2de3b2928cbfdf47b2a631fd0a
Ruchit Parmar <97833164+Ruchitparmar2012@users.noreply.github.com>
2023-10-04 15:27:15 +0530

    Update SQL To Terraform Function.py
    update the code and add the python and JS function

--- SQL To Terraform Function.py
+++ SQL To Terraform Function.py
@@ -1,3 +1,59 @@
+import os
+import re
+import time  # Import the time module
+
+# Get the current time in microseconds before starting execution
+start_time = time.perf_counter()
+
+# Get the current time in microseconds after finishing execution
+end_time = time.perf_counter()
+
+# Calculate the elapsed time in microseconds
+elapsed_time_microseconds = (end_time - start_time) * 1e6  # Convert to microseconds
+
+# Print the elapsed time in microseconds
+print(f"Elapsed time: {elapsed_time_microseconds:.2f} microseconds")
+
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Specify the relative folder path containing .sql files
+relative_folder_path = 'SQL_Files/Function'
+
+# Combine the current working directory with the relative folder path
+folder_path = os.path.join(current_directory, relative_folder_path)
+
+try:
+    # Get a list of all files in the folder
+    files = os.listdir(folder_path)
+
+    # Filter out only the .sql files
+    sql_files = [file for file in files if file.endswith('.sql')]
+
+    # Read the contents of each .sql file and store them in a list
+    sql_contents_list = []
+    for sql_file in sql_files:
+        file_path = os.path.join(folder_path, sql_file)
+        with open(file_path, 'r') as file:
+            sql_contents = file.read()
+            sql_contents_list.append(sql_contents)
+            
+except FileNotFoundError:
+    print(f"Folder not found: {folder_path}")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+import re
+# this code remove double quotes outside form DDL / Including Database, schema, table name 
+def remove_outer_quotes(sql):
+    ls1 = sql.split("(")[0].replace('"','')
+    ls2 = ["("+i for i in sql.split("(")[1:]] 
+    ls2.insert(0,ls1)
+    sql = "".join(ls2)  
+    
+    return sql
+
 
 
 import re
@@ -88,112 +144,289 @@ def python_terraform(sql):
             if comment_match:
                     comment_got = comment_match.group(1).strip()
 
-            
+            # regex for HANDLER
+            HANDLER_match = re.search(r'HANDLER\s*=\s*["\']([^"\']+)[\'"]', command, re.DOTALL | re.IGNORECASE)
+
+            if HANDLER_match:
+                HANDLER_value = HANDLER_match.group(1).strip()
+            else :
+                pass
+
+            # find the RUNTIME VERSION
+            RUNTIME_VERSION_match = re.search(r'RUNTIME_VERSION\s*=\s*["\']([^"\']+)[\'"]', sql, re.DOTALL | re.IGNORECASE)
+
+            if RUNTIME_VERSION_match:
+                RUNTIME_VERSION_value = RUNTIME_VERSION_match.group(1).strip()
+            else :
+                pass
+                
             # Use re.search() to find the first occurrence of the pattern in the SQL code
-            statemen_match = re.search(r"(AS '(?:[^']|''|[^;])*';)", sql, re.DOTALL)
-            
+            statement_match = re.search(r"AS\s+'(.*?)';", sql, re.DOTALL)
+
             # Check if there's a match before accessing the group attribute
-            if statemen_match:
-                statement_matches = statemen_match.group(0).strip("AS '").strip("';")
+            if statement_match:
+                statement_matches = statement_match.group(1)
                 extracted_code_single_quotes = statement_matches.replace("''", "'")
                 extracted_code_replaced = re.sub(r'(_PROD|_DEV)', r'_${var.SF_ENVIRONMENT}', extracted_code_single_quotes)
+                # print(extracted_code_replaced)
             else:
-                pass
+                print('none')
                 
             sql = sql.upper()
 
-            
-
-            # main code 
-            resource_table_name = f"resource \"snowflake_procedure\" \"{dynamic__main_db}_{schema_name}_{table_name}\""
-            code += f"{resource_table_name} {{\n"
-            code += f"\tname = \"{table_name}\"\n"
-            code += f"\tdatabase = \"{dynamic_db}\"\n"
-            code += f"\tschema = \"{schema_name}\"\n"
-            resource_table_name_demo = f'{dynamic__main_db}_{schema_name}_{table_name}'
-            resource_table_name_list.append(resource_table_name_demo)
-            code += f"\tlanguage  = \"{language_value}\"\n"
-
-            argument_name = []
-            argument_type = []
-            for argument in arguments:
-
-                    argument_name.append(argument["name"])
-                    argument_type.append(argument["type"])
-
-            for i in zip(argument_name,argument_type):
-                code += f"\n\targuments {{\n"
-                code += f"\t\tname = \"{i[0]}\"\n"
-                code += f"\t\ttype = \"{i[1]}\"\n"
-                code += "}\t\n"
-
-            if "COMMENT" in sql:
-                try:
-                        code += f"\tcomment = \"{comment_got}\"\n"
-                except AttributeError:
-                    pass
-            elif "COMMENT" not in sql:
-                pass
+            # this is for sql langauge 
+            if language_value=='SQL':
                 
-            if "RETURNS" in sql:
-                try:
-                    return_type_value = return_type_value.replace('"','')
-                    code += f"\treturn_type = \"{return_type_value}\"\n"
+                # main code 
+                resource_table_name = f"resource \"snowflake_procedure\" \"{dynamic__main_db}_{schema_name}_{table_name}\""
+                code += f"{resource_table_name} {{\n"
+                code += f"\tname = \"{table_name}\"\n"
+                code += f"\tdatabase = \"{dynamic_db}\"\n"
+                code += f"\tschema = \"{schema_name}\"\n"
+                resource_table_name_demo = f'{dynamic__main_db}_{schema_name}_{table_name}'
+                resource_table_name_list.append(resource_table_name_demo)
+                code += f"\tlanguage  = \"{language_value}\"\n"
+    
+                argument_name = []
+                argument_type = []
+                for argument in arguments:
+    
+                        argument_name.append(argument["name"])
+                        argument_type.append(argument["type"])
+    
+                for i in zip(argument_name,argument_type):
+                    code += f"\n\targuments {{\n"
+                    code += f"\t\tname = \"{i[0]}\"\n"
+                    code += f"\t\ttype = \"{i[1]}\"\n"
+                    code += "}\t\n"
+    
+                if "COMMENT" in sql:
+                    try:
+                            code += f"\tcomment = \"{comment_got}\"\n"
+                    except AttributeError:
+                        pass
+                elif "COMMENT" not in sql:
+                    pass
                     
-                except AttributeError:
+                if "RETURNS" in sql:
+                    try:
+                        return_type_value = return_type_value.replace('"','')
+                        code += f"\treturn_type = \"{return_type_value}\"\n"
+                        
+                    except AttributeError:
+                        pass
+                elif "RETURNS" not in sql:
+                    pass  
+    
+                if "NULL_INPUT_BEHAVIOR" in sql:
+                    try:
+                        null_input_behavior = null_input_behavior.replace('"','')
+                        code += f"\tnull_input_behavior  = \"{null_input_behavior}\"\n"
+                    except AttributeError:
+                        pass
+                elif "NULL_INPUT_BEHAVIOR" not in sql:
                     pass
-            elif "RETURNS" not in sql:
-                pass  
+                    
+                if "RETURN_BEHAVIOR" in sql:
+                    try:
+                        return_behavior_value = return_behavior_value.replace('"','')
+                        code += f"\treturn_behavior = \"{return_behavior_value}\"\n"
+                    except AttributeError:
+                        pass
+                elif "RETURN_BEHAVIOR" not in sql:
+                    pass
+    
+                code += f"\tstatement =  \"{extracted_code_replaced}\"\n"
+    
+                code += "}\n\n"
+            # this is for JAVASCRIPT langauge   
+            elif language_value=='JAVASCRIPT':
 
-            if "NULL_INPUT_BEHAVIOR" in sql:
-                try:
-                    null_input_behavior = null_input_behavior.replace('"','')
-                    code += f"\tnull_input_behavior  = \"{null_input_behavior}\"\n"
-                except AttributeError:
+                # main code 
+                resource_table_name = f"resource \"snowflake_procedure\" \"{dynamic__main_db}_{schema_name}_{table_name}\""
+                code += f"{resource_table_name} {{\n"
+                code += f"\tname = \"{table_name}\"\n"
+                code += f"\tdatabase = \"{dynamic_db}\"\n"
+                code += f"\tschema = \"{schema_name}\"\n"
+                resource_table_name_demo = f'{dynamic__main_db}_{schema_name}_{table_name}'
+                resource_table_name_list.append(resource_table_name_demo)
+                code += f"\tlanguage  = \"{language_value}\"\n"
+    
+                argument_name = []
+                argument_type = []
+                for argument in arguments:
+    
+                        argument_name.append(argument["name"])
+                        argument_type.append(argument["type"])
+    
+                for i in zip(argument_name,argument_type):
+                    code += f"\n\targuments {{\n"
+                    code += f"\t\tname = \"{i[0]}\"\n"
+                    code += f"\t\ttype = \"{i[1]}\"\n"
+                    code += "}\t\n"
+    
+                if "COMMENT" in sql:
+                    try:
+                            code += f"\tcomment = \"{comment_got}\"\n"
+                    except AttributeError:
+                        pass
+                elif "COMMENT" not in sql:
                     pass
-            elif "NULL_INPUT_BEHAVIOR" not in sql:
-                pass
+                    
+                if "RETURNS" in sql:
+                    try:
+                        return_type_value = return_type_value.replace('"','')
+                        code += f"\treturn_type = \"{return_type_value}\"\n"
+                        
+                    except AttributeError:
+                        pass
+                elif "RETURNS" not in sql:
+                    pass  
+    
+                if "NULL_INPUT_BEHAVIOR" in sql:
+                    try:
+                        null_input_behavior = null_input_behavior.replace('"','')
+                        code += f"\tnull_input_behavior  = \"{null_input_behavior}\"\n"
+                    except AttributeError:
+                        pass
+                elif "NULL_INPUT_BEHAVIOR" not in sql:
+                    pass
+                    
+                if "RETURN_BEHAVIOR" in sql:
+                    try:
+                        return_behavior_value = return_behavior_value.replace('"','')
+                        code += f"\treturn_behavior = \"{return_behavior_value}\"\n"
+                    except AttributeError:
+                        pass
+                elif "RETURN_BEHAVIOR" not in sql:
+                    pass
+
+                if "HANDLER" in sql:
+                    try:
+                        code += f"\thandler= \"{HANDLER_value}\"\n"
+                    except AttributeError:
+                        pass
+                elif "HANDLER" not in sql:
+                    pass
                 
-            if "RETURN_BEHAVIOR" in sql:
-                try:
-                    return_behavior_value = return_behavior_value.replace('"','')
-                    code += f"\treturn_behavior = \"{return_behavior_value}\"\n"
-                except AttributeError:
+                
+                code += f"\tstatement =  \"{extracted_code_replaced}\"\n"
+    
+                code += "}\n\n"
+
+            # this is for python language 
+            else:
+
+                # main code 
+                resource_table_name = f"resource \"snowflake_procedure\" \"{dynamic__main_db}_{schema_name}_{table_name}\""
+                code += f"{resource_table_name} {{\n"
+                code += f"\tname = \"{table_name}\"\n"
+                code += f"\tdatabase = \"{dynamic_db}\"\n"
+                code += f"\tschema = \"{schema_name}\"\n"
+                resource_table_name_demo = f'{dynamic__main_db}_{schema_name}_{table_name}'
+                resource_table_name_list.append(resource_table_name_demo)
+                code += f"\tlanguage  = \"{language_value}\"\n"
+    
+                argument_name = []
+                argument_type = []
+                for argument in arguments:
+    
+                        argument_name.append(argument["name"])
+                        argument_type.append(argument["type"])
+    
+                for i in zip(argument_name,argument_type):
+                    code += f"\n\targuments {{\n"
+                    code += f"\t\tname = \"{i[0]}\"\n"
+                    code += f"\t\ttype = \"{i[1]}\"\n"
+                    code += "}\t\n"
+    
+                if "COMMENT" in sql:
+                    try:
+                            code += f"\tcomment = \"{comment_got}\"\n"
+                    except AttributeError:
+                        pass
+                elif "COMMENT" not in sql:
                     pass
-            elif "RETURN_BEHAVIOR" not in sql:
-                pass
+                    
+                if "RETURNS" in sql:
+                    try:
+                        return_type_value = return_type_value.replace('"','')
+                        code += f"\treturn_type = \"{return_type_value}\"\n"
+                        
+                    except AttributeError:
+                        pass
+                elif "RETURNS" not in sql:
+                    pass  
+    
+                if "NULL_INPUT_BEHAVIOR" in sql:
+                    try:
+                        null_input_behavior = null_input_behavior.replace('"','')
+                        code += f"\tnull_input_behavior  = \"{null_input_behavior}\"\n"
+                    except AttributeError:
+                        pass
+                elif "NULL_INPUT_BEHAVIOR" not in sql:
+                    pass
+                    
+                if "RETURN_BEHAVIOR" in sql:
+                    try:
+                        return_behavior_value = return_behavior_value.replace('"','')
+                        code += f"\treturn_behavior = \"{return_behavior_value}\"\n"
+                    except AttributeError:
+                        pass
+                elif "RETURN_BEHAVIOR" not in sql:
+                    pass
 
-            code += f"\tstatement =  \"{extracted_code_replaced}\"\n"
+                if "RUNTIME_VERSION" in sql:
+                    try:
+                        code += f"\truntime_version = \"{RUNTIME_VERSION_value}\"\n"
+                    except AttributeError:
+                        pass 
+                elif "RUNTIME_VERSION" not in sql:
+                    pass
+                
+                if "HANDLER" in sql:
+                    try:
+                        code += f"\thandler= \"{HANDLER_value}\"\n"
+                    except AttributeError:
+                        pass
+                elif "HANDLER" not in sql:
+                    pass
+                    
+                code += f"\tstatement =  \"{extracted_code_replaced}\"\n"
+    
+                code += "}\n\n"
+                    
 
-            code += "}\n\n"
+        
 
            
                 
     return code
 
 
-sql = '''
-CREATE OR REPLACE FUNCTION DISC_PROD.DATA_CLEANSING.SSN_VALIDATION("STR_ETL_TASK_KEY" VARCHAR(16777216), "STR_CDC_START" VARCHAR(16777216), "STR_CDC_END" VARCHAR(16777216))
-RETURNS VARCHAR(12)
-COMMENT 'demoaa'
-LANGUAGE "SQL"
-NULL_INPUT_BEHAVIOR 111
-return_behavior number(1110)
-AS '
-     SELECT 
-     CASE 
-    WHEN TRIM(SSN) = ' OR SSN IS NULL THEN  NULL
-    WHEN LENGTH(REGEXP_REPLACE(TRIM(SSN),'[^[:digit:]$]'))<>9 THEN NULL
-        WHEN NOT (NULLIF(TRIM(REGEXP_REPLACE(SSN ,'\\-|\\\\\\\\s|\\\\\\\\\\\\\\\\|[A-Z]')),') LIKE ANY ('666%','000%','9%','%0000','___00%'))
-        THEN (CASE WHEN
-     CONTAINS(TRIM(SSN), '-') = FALSE  THEN CONCAT(SUBSTRING(SSN,1,3)||''-''||SUBSTRING(SSN,4,2)||'-'||SUBSTRING(SSN,6,4))
-     ELSE TRIM(SSN)
-     END)
-        ELSE  NULL 
-    END
-';
-'''
-sql_without_quotes = remove_outer_quotes(sql)
-main = python_terraform(sql_without_quotes)
-print(main)
+# Process each SQL content and generate Terraform code
+for sql_contents in sql_contents_list:
+    sql_without_quotes = remove_outer_quotes(sql_contents)
+    main = python_terraform(sql_without_quotes)
+#     print(main)
 
+output_folder = os.path.join(current_directory, 'Terraform_Files','Function')
+
+try:
+    os.makedirs(output_folder, exist_ok=True)
+except Exception as e:
+    print(f"An error occurred while creating the output folder: {e}")
+
+for i, sql_contents in enumerate(sql_contents_list):
+    sql_without_quotes = remove_outer_quotes(sql_contents)
+    main = python_terraform(sql_without_quotes)
+
+    for i in resource_table_name_list:
+        resource_name = i 
+        output_filename = os.path.join(output_folder, f"{resource_name}.tf")
+
+    try:
+        with open(output_filename, 'w') as tf_file:
+            tf_file.write(main)
+    except Exception as e:
+        print(f"An error occurred while writing the output file: {e}")
